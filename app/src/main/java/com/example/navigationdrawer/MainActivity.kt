ee622bd520ea82dfc9da1e33869f1157f983b7ce
aungaunglwin99 <aungaunglwin2061999@gmail.com>
2026-01-18 22:44:38 +0900

    FullDone

--- app/src/main/java/com/example/navigationdrawer/MainActivity.kt
+++ app/src/main/java/com/example/navigationdrawer/MainActivity.kt
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var dlMain: DrawerLayout
     private lateinit var nvMain: NavigationView
     private lateinit var tbToolbar: Toolbar
@@ -20,13 +22,31 @@ class MainActivity : AppCompatActivity() {
         nvMain = findViewById(R.id.nvMain)
         tbToolbar = findViewById(R.id.tbToolbar)
 
-        tbToolbar.setNavigationOnClickListener{
-            if(dlMain.isDrawerOpen(GravityCompat.START)){
+        // Default Fragment
+        loadFragment(MyAccountFragment())
+
+        tbToolbar.setNavigationOnClickListener {
+            if (dlMain.isDrawerOpen(GravityCompat.START)) {
                 dlMain.closeDrawer(GravityCompat.START)
-            }  else{
+            } else {
                 dlMain.openDrawer(GravityCompat.START)
             }
         }
 
+        nvMain.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.mitem_account -> loadFragment(MyAccountFragment())
+                R.id.mitem_setting -> loadFragment(SettingsFragment())
+                R.id.mitem_logout -> finish()
+            }
+            dlMain.closeDrawer(GravityCompat.START)
+            true
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
     }
 }
